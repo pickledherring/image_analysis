36a0c5d720f2c0a7cba9011a0e936e7ae5eb2b7d
Gabriel Salmon <gtsalmon@gmail.com>
2022-05-11 02:01:33 -0400

    just need to run and debug

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ import matplotlib.pyplot as plt
 import math
 import statistics as st
 import random
+import copy
+import csv
 import time
-from numpy import arange
+from re import search
+from numpy import arange, ravel, array
 from PIL import Image, ImageFile
 
 ImageFile.LOAD_TRUNCATED_IMAGES = True
@@ -67,7 +70,11 @@ def open_in_gray(file):
     pix = img.getdata()
     pix_list = []
     for y in range(height):
-        pix_list.append([pix[y * width + x] for x in range(width)])
+        row = []
+        for x in range(width):
+            value = pix[y * width + x]
+            row.append(value)
+        pix_list.append(row)
 
     # all functions are on grayscale, so this step is required
     pix_list = grayscale(pix_list)
@@ -77,8 +84,9 @@ def to_image(pix_list, save_loc=None):
     size = [len(pix_list[0]), len(pix_list)]
     flat_list = []
     for row in pix_list:
-        for pixel in row:
-            flat_list.append(pixel)
+        for i in range(len(row)):
+            value = row[i]
+            flat_list.append(value)
     output = Image.new("L", size)
     output.putdata(flat_list)
     
@@ -182,6 +190,50 @@ def quantizer(img, num_levels):
     msqe /= len(img) * len(img[0])
 
     return new_img, msqe
+
+def make_bgnd_white_arrayify(img):
+    length = len(img)
+    width = len(img[0])
+    # evaluate img, if mostly black, flip every pixel
+    # I assume the background will occupy most of the image
+    sum_white = sum([sum(img[i]) for i in range(len(img))])
+    if sum_white / 255 < length * width / 2:
+        less = ravel(img) - 255
+        inverted = abs(less)
+        inverted = inverted.reshape((length, width))
+    else:
+        inverted = array(img)
+    return inverted
+
+def crop(img, amount):
+    # amount is width in pixels to crop from all edges
+    length = len(img)
+    width = len(img[0])
+    new_img = []
+    for i in range(length - 2 * amount):
+        new_img.append([])
+        for j in range(width - 2 * amount):
+            value = img[amount+i][amount+j]
+            new_img[i].append(value)
+
+    return new_img
+
+def pad(img, amount):
+    # amount is width in pixels to pad on all edges
+    length = len(img)
+    width = len(img[0])
+    new_img = []
+    for i in range(length + 2 * amount):
+        new_img.append([])
+        for j in range(width + 2 * amount):
+            if i >= length + amount or j >= width + amount\
+                or i < amount or j < amount:
+                value = 255
+            else:
+                value = img[i-amount][j-amount]
+            new_img[i].append(value)
+
+    return new_img
 
 # Filtering operations:
 # *ext_bounds helps the linear filters by extending the image past
@@ -668,7 +720,280 @@ dilate_erode_weights = [[1, 1, 1, 1, 1],
                         [1, 1, 1, 1, 1],
                         [1, 1, 1, 1, 1]]
 
+def dbscan_seg(img, radius=10, min_obj=60):
+    length = len(img)
+    width = len(img[0])
+    avg_dim = (length + width) / 2
+
+    new_img = [[0 for _ in range(width)] for _ in range(length)]
+
+    cores = []
+    for i in range(length):
+        for j in range(width):
+            n_nearby = 0
+            nearby = []
+            # searching within a circle within a square - there's probably a better way to do this
+            for m in range(-radius, radius):
+                for n in range(-radius, radius):
+                    if i + m >= 0 and j + n >= 0 and i + m < length and j + n < width:
+                        # scale the intensity distance to image dimensions
+                        int_dist = (img[i][j] - img[i+m][j+n]) * avg_dim / 255
+                        if math.sqrt(m**2 + n**2 + int_dist**2) <= radius:
+                            n_nearby += 1
+                            nearby.append([i + m, j + n])
+                            new_img[i+m][j+n] = -1
+
+            if n_nearby >= min_obj:
+                cores.append({"loc": [i, j], "neighbors": nearby})
+                new_img[i][j] = -1
+
+    clusters = {}
+    cluster_serial = 0
+    for core in cores:
+        # 0 for the pixel value will stand in for "background", 1, 2, 3, etc. will be cluster #s
+        # if we find a background core, make it the start of a new cluster
+        i = core["loc"][0]
+        j = core["loc"][1]
+        if new_img[i][j] == -1:
+            new_img[i][j] = cluster_serial
+            clusters[str(cluster_serial)] = [[i, j]]
+            cluster_serial += 1
+        # go through the neighbors of the core and add them to the core's cluster if they are background
+        for point in core["neighbors"]:
+            m = point[0]
+            n = point[1]
+            if new_img[m][n] == -1:
+                new_img[m][n] = new_img[i][j]
+                clusters[str(new_img[i][j])].append([m, n])
+
+            # if we find a point belonging to a different cluster, add this cluster to that one
+            elif new_img[m][n] != new_img[i][j]:
+                temp = str(new_img[i][j])
+                for nb in clusters[str(new_img[i][j])]:
+                    new_img[nb[0]][nb[1]] = new_img[m][n]
+
+                clusters[str(new_img[m][n])].extend(clusters[temp])
+                clusters.pop(temp)
+
+    return clusters
+
+def get_perimeter(seg_img):
+    cross_weights = [[0, 1, 0], [1, 1, 1], [0, 1, 0]]
+    dilated = dilate(seg_img, weights=cross_weights)
+    dilated_1d = ravel(dilated) + 255
+    seg_1d = ravel(seg_img)
+    outlines = dilated_1d - seg_1d
+    negative = outlines - 255
+    return -sum(negative) / 255
+
+def get_area_bbox_com(clusters):
+    areas = []
+    bboxs = []
+    coms = []
+    for cluster_k in clusters.keys():
+        num_points = len(clusters[cluster_k])
+        areas.append(num_points)
+
+        j_sum = k_sum = j_max = k_max = 0
+        j_min = k_min = 100000
+        for i in range(num_points):
+            j = clusters[cluster_k][i][0]
+            k = clusters[cluster_k][i][1]
+            j_sum += j
+            k_sum += k
+            if j < j_min:
+                j_min = j
+            if k < k_min:
+                k_min = k
+            if j > j_max:
+                j_max = j
+            if k > k_max:
+                k_max = k
+
+        # bounding boxes have shape [height, width]
+        bbox_top_left = [j_min, k_min]
+        bbox_bottom_right = [j_max, k_max]
+
+        center_of_mass = [round(j_sum / num_points), round(k_sum / num_points)]
+
+        bboxs.append([bbox_top_left, bbox_bottom_right])
+        coms.append(center_of_mass)
+    
+    return areas, bboxs, coms
+
+def draw_bboxs(img, bboxs):
+    img_draw = copy.deepcopy(img)
+    for i in range(len(bboxs)):
+        br_y = bboxs[i][1][0]
+        tl_y = bboxs[i][0][0]
+        br_x = bboxs[i][1][1]
+        tl_x = bboxs[i][0][1]
+        for j in range(br_y - tl_y):
+            img_draw[tl_y+j][tl_x] = 125
+            img_draw[tl_y+j][br_x] = 125
+        for k in range(br_x - tl_x):
+            img_draw[tl_y][tl_x+k] = 125
+            img_draw[br_y][tl_x+k] = 125
+
+    img_1d = ravel(img_draw)
+    output = Image.new("L", [len(img[0]), len(img)])
+    output.putdata(img_1d)
+    return output
+
+def cut_clusters(img, bboxs):
+    # returns list of image patches from bounding boxes
+    new_images = []
+    for i in range(len(bboxs)):
+        new_images.append([])
+        br_i = bboxs[i][1][0]
+        tl_i = bboxs[i][0][0]
+        br_j = bboxs[i][1][1]
+        tl_j = bboxs[i][0][1]
+        for j in range(tl_i, br_i + 1):
+            new_images[i].append([])
+            for k in range(tl_j, br_j + 1):
+                try:
+                    new_images[i][j-tl_i].append(img[j][k])
+                except:
+                    print(i, j, k)
+
+    
+    return new_images
+
+def get_centroid(img):
+    height = len(img)
+    width = len(img[0])
+
+    flip_sum_ix = flip_sum_jx = flip_area = 0
+    for i in range(height):
+        for j in range(width):
+            flip_sum_ix += (img[i][j] - 255) * i
+            flip_sum_jx += (img[i][j] - 255) * j
+            flip_area += img[i][j] - 255
+    area = flip_area
+    return [flip_sum_ix / area, flip_sum_jx / area]
+
+def cent_moment(img, com, p, q):
+    # not entirely sure this is correct
+    height = len(img)
+    width = len(img[0])
+    summed = 0
+    for i in range(height):
+        for j in range(width):
+            value = abs(img[i][j] - 255) / 255
+            summed += (i - com[0])**p * (j - com[1])**q * value
+    return summed
+
+def get_cms_orient(img, com):
+    cm_00 = cent_moment(img, com, 0, 0)
+    cm_11 = cent_moment(img, com, 1, 1)
+    cm_02 = cent_moment(img, com, 0, 2)
+    cm_20 = cent_moment(img, com, 2, 0)
+
+    cm_20_prime = cm_20 / cm_00
+    cm_02_prime = cm_02 / cm_00
+    cm_11_prime = cm_11 / cm_00
+
+    theta = 1/2 * math.atan(2 * cm_11_prime / (cm_20_prime - cm_02_prime))
+
+    return cm_11, cm_02, cm_20, theta
+
+def get_feats(img):
+    # all the feature extraction and processing for that, takes a grayscale array image
+    # get rid of annoying line artifacts on the edges
+    crop_img = copy.deepcopy([x[3:-3] for x in img[3:-3]])
+    # binarize image
+    seg = k_means_quantize(crop_img, 2)
+    dilate_erode_weights = [[1, 1, 1], [1, 1, 1], [1, 1, 1]]
+    # clean it up a bit
+    eroded = erode(seg, weights=dilate_erode_weights)
+    clean = dilate(eroded, weights=dilate_erode_weights)
+
+    # make the largest cluster, probably the background, white so dbscan_seg will ignore it
+    clean = make_bgnd_white_arrayify(clean)
+    # print("image cleaned, clustering (will take a while)")
+
+    # get object clusters (pixel locations) from image
+    clusters = dbscan_seg(clean)
+    # print("done clustering! extracting cells and features")
+    
+    # get the background cluster out of there
+    avg_intensities = []
+    keys = []
+    for cluster_k in clusters.keys():
+        num_points = len(clusters[cluster_k])
+        sum_intensity = 0
+        for i in range(num_points):
+            j = clusters[cluster_k][i][0]
+            k = clusters[cluster_k][i][1]
+            sum_intensity += clean[j][k]
+        avg_intensities.append(sum_intensity / num_points)
+        keys.append((cluster_k))
+
+    max_index = avg_intensities.index(max(avg_intensities))
+    clusters.pop(keys[max_index])
+
+    # start retrieving features
+    areas, bboxs, coms = get_area_bbox_com(clusters)
+    height = len(img)
+    width = len(img[0])
+
+    # get square patches from segmented image
+    patches = cut_clusters(clean, bboxs)
+    perimeters = []
+    centroids = []
+    cms_11 = []
+    cms_20 = []
+    cms_02 = []
+    orientations = []
+    for i, patch in enumerate(patches):
+        # pad image so dilation to get perimeter works
+        padded = pad(patch, 3)
+        perimeters.append(get_perimeter(padded))
+
+        # get centroid and center of mass
+        centroid = get_centroid(patch)
+        centroids.append([centroid[0] / len(patch), centroid[1] / len(patch[0])])
+
+        coms[i] = [coms[i][0] / height, coms[i][1] / width]
+
+        # 1st and 2nd central moments and orientation
+        cm_11, cm_02, cm_20, theta = get_cms_orient(patch, coms[i])
+        cms_11.append(cm_11)
+        cms_20.append(cm_20)
+        cms_02.append(cm_02)
+        orientations.append(theta)
+
+    return (areas, bboxs, coms, perimeters, centroids, cms_11,
+    cms_20, cms_02, orientations)
+
 # abbrs = ["cyl", "para", "inter", "super", "let", "mod", "svar"]
 # for abbr in abbrs:
 #     to_image(edge_operator(open_in_gray(f"Cancerous cell smears/{abbr}01.BMP"), sharpen_thresh=5),
         # save_loc=f"outputs/{abbr}_sharp.png")
+
+# make a csv, takes several hours
+with open("cell_data.csv", 'w') as f:
+    cells_writer = csv.writer(f)
+    cells_writer.writerow(['area', 'bbox', 'com', 'perimeter', 'centroid', 'cm_11',
+                            'cm_20', 'cm_02', 'orientation', 'class'])
+    
+    paths = glob.glob(f"Cancerous cell smears/*")
+    for index, path in enumerate(paths):
+        if index % 5 == 0:
+            print(f"processed {index} images")
+        img = open_in_gray(path)
+        feats = get_feats(img)
+        for i in range(len(feats[1])):
+            values = []
+            # returning multiple objects per feature, need to invert order
+            for j in range(len(feats)):
+                values.append(feats[j][i])
+            # add class based on file name
+            values.append(search('/\D*', path).group(0)[1:])
+            cells_writer.writerow(values)
+
+
+
+
+
